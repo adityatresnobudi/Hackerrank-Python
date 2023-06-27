b825e158a7128b71a17aded3ca19a203b628eeae
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-06-27 12:35:21 +0700

    Update itertools.product().py

--- Easy/itertools.product().py
+++ Easy/itertools.product().py
@@ -4,10 +4,4 @@ from itertools import product
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
-print(*product(A,B))# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import product
-
-A = list(map(int,input().split()))
-B = list(map(int,input().split()))
-
 print(*product(A,B))
