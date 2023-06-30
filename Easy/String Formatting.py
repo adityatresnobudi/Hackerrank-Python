782403b4b8a3b6d1cfde17da42c0732e2959d4d9
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-06-30 19:51:09 +0700

    Delete Easy directory

--- Easy/String Formatting.py
+++
@@ -1,9 +0,0 @@
-def print_formatted(number):
-    # your code goes here
-    s = len(str(bin(number)).replace("0b",""))
-    for i in range (1, number+1):
-        print(f"{str(i).rjust(s,' ')} {oct(i)[2:].rjust(s,' ')} {hex(i)[2:].upper().rjust(s,' ')} {bin(i)[2:].rjust(s,' ')}")
-
-if __name__ == '__main__':
-    n = int(input())
-    print_formatted(n)
