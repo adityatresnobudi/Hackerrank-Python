b2bd639e7147c6ad58886871011be8b9e9c9221c
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-07-14 20:44:16 +0700

    Create Find Angle MBC.py

---
+++ Find Angle MBC.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+AB = int(input())
+BC = int(input())
+theta = round(math.degrees(math.atan(AB / BC)))
+print(theta, chr(176), sep = '')
