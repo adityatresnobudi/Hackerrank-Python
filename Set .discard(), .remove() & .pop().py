5e97362ad8c539dad27643c95d1f20c4155b1444
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-07-14 21:48:14 +0700

    Create Set .discard(), .remove() & .pop().py

---
+++ Set .discard(), .remove() & .pop().py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int, input().split()))
+N = int(input())
+
+for i in range(N) :
+    c = input().split()
+    if c[0] == 'pop':
+        s.pop()
+    elif c[0] == 'remove':
+        s.remove(int(c[1]))
+    elif c[0] == 'discard':
+        s.discard(int(c[1]))
+    else:
+        continue
+        
+print(sum(s))
